da757d9f121d1f22e35c4b0ccb66ea9177d15514
Catarina Lima <catarinalimalss@gmail.com>
2022-03-21 20:23:46 -0300

    Create questao02.py

---
+++ questao02.py
@@ -0,0 +1,7 @@
+# Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo. 
+valor = int(input('Digite um número positivo ou negativo: '))
+
+if valor < 0:
+    print ('O numero é negativo!')
+else:
+    print ('O numero é positivo!')
