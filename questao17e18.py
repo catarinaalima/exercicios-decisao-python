a8863b70ee3185955ba67fdc7d36d164d63fc9ed
Catarina Lima <catarinalimalss@gmail.com>
2022-03-21 21:09:47 -0300

    Create questao17e18.py

---
+++ questao17e18.py
@@ -0,0 +1,51 @@
+# Faça um Programa que peça um número correspondente a um determinado ano e em seguida informe se este ano é ou não bissexto.
+
+
+#Anos bissextos são aqueles que são múltiplos de 4, como 1996, 2000, 2004 etc (que podem ser divididos por 4 deixando resto 0).
+#Porém, há uma exceção: múltiplos de 100 que não sejam múltiplos de 400.
+
+#Uma das duas condições a seguir deve ser verdadeira:
+#Condição 1: Ser múltiplo de 4 e não ser múltiplo de 100
+#Condição 2: Ser múltiplo de 400 (se for múltiplo de 400 automaticamente é de 4)
+
+ano = int(input('Ano: '))
+
+if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+    print('Bissexto!')
+else:
+    print('Não é bissexto!')
+
+    
+
+    
+    
+    
+# Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a mesma é uma data válida.
+
+data = input("Digite uma data com o seguinte formato dd/mm/aaaa: ")
+dia = int(data[0:2])
+mes = int(data[3:5])
+ano = int(data[6:10])
+
+validade = "true"
+i = 0
+while validade == "true" and i == 0:
+    if (ano % 4 == 0 and ano % 100 != 0) or ano % 400 == 0:
+        bissexto = "sim"
+    else:
+        bissexto = "nao"
+
+    if mes < 1 or mes > 12:
+        validade = "false"
+
+    if dia > 31 or ((mes == 4 or mes == 6 or mes == 9 or mes == 11) and dia > 30):
+        validade = "false"
+
+    if (mes == 2 and bissexto == "nao" and dia > 28) or ( mes == 2 and bissexto == "sim" and dia > 29):
+        validade = "false"
+    i = i + 1
+
+if validade == "true":
+    print("Data válida!")
+else:
+    print("Data inválida!")
